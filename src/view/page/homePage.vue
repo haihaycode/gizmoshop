8a156b792cae96fc477316b9f065e9dad936476a
haihaycode <kienthuc177@gmail.com>
2024-11-09 16:36:37 +0700

    custom ui

--- src/view/page/homePage.vue
+++ src/view/page/homePage.vue
@@ -1,32 +1,21 @@
 <template>
-    <div class="p-2 bg-gray-200 bg-opacity-30 shadow-md shadow-black/5  lg:m-2 md:m-0 h-screen ">
-        <h1 class="text-lg font-semibold mb-4 lg:mb-0 lg:text-xl">Dashboard - GizmoShop</h1>
-        <div class="space-y-2 text-sm lg:text-base">
-            <p><strong>Token:</strong> <span class="break-all">{{ token }}</span></p>
-            <p><strong>Refresh Token:</strong> <span class="break-all">{{ refreshToken }}</span></p>
-            <p><strong>Email:</strong> {{ email }}</p>
-            <p><strong>Role:</strong> {{ role }}</p>
-        </div>
+    <div class="md:m-0 ">
+        <carouselVoucherComponent></carouselVoucherComponent>
+        <voucherHomeComponent></voucherHomeComponent>
+        <TopDiscountedProductsComponent></TopDiscountedProductsComponent>
     </div>
 </template>
-
 <script>
-import { mapGetters } from 'vuex';
 
+import carouselVoucherComponent from '@/components/carouselHome/carouselVoucherComponent.vue';
+import voucherHomeComponent from '@/components/carouselHome/voucherHomeComponent.vue';
+import TopDiscountedProductsComponent from '@/components/product/TopDiscountedProductsComponent.vue';
 export default {
-    name: 'homePage',
-    computed: {
-        ...mapGetters('auth', ['token', 'refreshToken', 'name', 'role', 'user']),
-        email() {
-            return this.name || 'Không có email';
-        }
-    },
-    mounted() {
-        console.log('User Info:', this.user);
+    name: 'demoTestDev',
+    components: {
+        carouselVoucherComponent,
+        voucherHomeComponent,
+        TopDiscountedProductsComponent
     }
 };
 </script>
-
-<style scoped>
-/* Add custom styles if necessary */
-</style>
