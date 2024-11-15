bfd1765b8ef8df380741ac1b4bcd607517426cac
thansex000 <loclop3@gmail.com>
2024-11-15 19:01:52 +0700

    voucher detail config

--- src/api/voucherApi.js
+++ src/api/voucherApi.js
@@ -3,9 +3,11 @@ import { HOST } from '@/api/config'
 
 
 // /api/public/voucher/getallforuser/page
-export const getVoucherPage = async () => {
+export const getVoucherPage = async (data) => {
     try {
-        const response = await Axios.get(`${HOST}/api/public/voucher/getallforuser/page`);
+        const response = await Axios.get(`${HOST}/api/public/voucher/getallforuser/page`, {
+            params: data,
+        });
         return response.data;
     } catch (error) {
         throw new Error(`${error}`);
