bc5a290a4111106238ab7b37168152258f44702c
haihaycode <kienthuc177@gmail.com>
2024-12-16 15:28:24 +0700

    config ui

--- src/view/layout/MemberLayout.vue
+++ src/view/layout/MemberLayout.vue
@@ -6,10 +6,13 @@
     <main class="w-full mx-auto container min-h-screen transition-all main relative ">
       <router-view name="sidebar"></router-view>
       <section class="main xl:max-w-7xl mx-auto ">
-        <router-view></router-view>
+        <transition name="fade" @before-enter="beforeEnter" @enter="enter" @leave="leave">
+          <router-view></router-view>
+        </transition>
       </section>
       <ScrollToTopComponent />
     </main>
+    <!-- navigation moblie  -->
     <router-view name="mobile" class="block md:hidden"></router-view>
     <!-- Footer -->
     <router-view name="footer" class="footer"></router-view>
@@ -28,5 +31,33 @@ export default {
   computed: {
     ...mapGetters('loading', ['isLoading']),
   },
+  methods: {
+    beforeEnter(el) {
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      el.offsetHeight;
+      el.style.transition = "opacity 1s ease";
+      el.style.opacity = 1;
+      done();
+    },
+    leave(el, done) {
+      el.style.transition = "opacity 1s ease";
+      el.style.opacity = 0;
+      done();
+    }
+  },
+
 };
 </script>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/view/layout/SupplierLayout.vue
+++ src/view/layout/SupplierLayout.vue
@@ -8,7 +8,9 @@
             <router-view name="sidebar"></router-view>
             <!-- Main Content -->
             <main class="p-0 md:p-6 bg-white min-h-screen w-full shadow-inner ">
-                <router-view />
+                <transition name="fade" @before-enter="beforeEnter" @enter="enter" @leave="leave">
+                    <router-view></router-view>
+                </transition>
             </main>
         </div>
 
@@ -22,9 +24,32 @@
 
 export default {
     name: 'SupplierLayout',
+    methods: {
+        beforeEnter(el) {
+            el.style.opacity = 0;
+        },
+        enter(el, done) {
+            el.offsetHeight;
+            el.style.transition = "opacity 1s ease";
+            el.style.opacity = 1;
+            done();
+        },
+        leave(el, done) {
+            el.style.transition = "opacity 1s ease";
+            el.style.opacity = 0;
+            done();
+        }
+    }
 };
 </script>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1s ease;
+}
 
-<style>
-/* Additional custom styles can go here */
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
 </style>
